ed675ba332632833ba66c28844b350b3e683cf44
= <kamponistullar@gmail.com>
2021-12-09 17:16:57 +0100

    final file save :wrench: :fire:

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,27 +1,12 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: '0.8.0',
   networks: {
     rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/dtY67tvO8iwsMm25ZN5u3le8xwZHA7UP',
-      accounts: ['88d1462d30c7004dd558eb2e9dd231ba58665cf54de08fd597c46e183f869512'],
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
--- scripts/run.js
+++ scripts/run.js
@@ -1,36 +1,46 @@
 const main = async () => {
-    const [sender, friend] = await hre.ethers.getSigners();
-    const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
-    const waveContract = await waveContractFactory.deploy();
-    await waveContract.deployed();
+  const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther('0.1'),
+  });
+  await waveContract.deployed();
+  console.log('Contract addy:', waveContract.address);
 
-    console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", sender.address);
+  let contractBalance = await hre.ethers.provider.getBalance(
+    waveContract.address
+  );
+  console.log(
+    'Contract balance:',
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
-    console.log(`Contract signed between ${sender.address} and ${friend.address}`)
+  /*
+   * Let's try two waves now
+   */
+  const waveTxn = await waveContract.wave('This is wave #1');
+  await waveTxn.wait();
 
-    let totalWave;
-    totalWave = await waveContract.getTotalWaves();
-  
-    let waveTxn = await waveContract.wave();
-    await waveTxn.wait();
+  const waveTxn2 = await waveContract.wave('This is wave #2');
+  await waveTxn2.wait();
 
-    totalWave = await waveContract.getTotalWaves();
+  contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+  console.log(
+    'Contract balance:',
+    hre.ethers.utils.formatEther(contractBalance)
+  );
 
-    waveTxn = await waveContract.connect(friend).wave();
-    await waveTxn.wait();
-
-    totalWave = await waveContract.getTotalWaves();
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
 };
 
 const runMain = async () => {
-    try {
-        await main();
-        process.exit(0);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 };
 
 runMain();
